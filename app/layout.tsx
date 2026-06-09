2ff6e2c291ad8ec39f3348a81611cc153b7226df
cldnce-droid <cldnce@gmail.com>
2026-06-09 14:37:53 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,31 @@ export const metadata: Metadata = {
     statusBarStyle: "default"
   },
   icons: {
-    icon: "/icon.svg",
-    apple: "/icon.svg"
+    icon: [
+      {
+        url: "/icons/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon"
+      },
+      {
+        url: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        url: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ],
+    shortcut: "/icons/favicon.ico",
+    apple: [
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png"
+      }
+    ]
   }
 };
 
