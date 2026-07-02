00e17fd528a628f4535280b971a92a8d3a0754ba
cldnce-droid <cldnce@gmail.com>
2026-07-01 22:44:28 -0300

    Update OneSignalInitializer.tsx

--- components/OneSignalInitializer.tsx
+++ components/OneSignalInitializer.tsx
@@ -1,19 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
-import {
-  getOneSignal,
-  isAppIdMismatch,
-  resetOneSignalRegistration
-} from "@/lib/onesignalClient";
+import { getOneSignal } from "@/lib/onesignalClient";
 
 export function OneSignalInitializer() {
   useEffect(() => {
-    void getOneSignal().catch(async (error) => {
-      if (!isAppIdMismatch(error)) return;
-      await resetOneSignalRegistration();
-      window.location.reload();
-    });
+    void getOneSignal().catch(() => undefined);
   }, []);
 
   return null;
